640231e6ad2dcedf02b48c076a96a4e3dd006053
douaâ <tijanidouaa2005@gmail.com>
2026-05-24 16:09:41 +0100

    initial commit

---
+++ app/Http/Controllers/Enseignant/AnnulationAbsenceController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Enseignant;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AnnulationAbsenceController extends Controller
+{
+    //
+}
